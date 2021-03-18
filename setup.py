836e834549fdd3e5237cf35b440a094eff04b4b3
James Ross <jross@bwh.harvard.edu>
2021-03-18 14:59:48 -0400

    Incrementing minor version to 0.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='provenance_tools',
-    version='0.0.2',
+    version='0.0.3',
     description='provenance_tools',
     long_description=long_description,
     long_description_content_type='text/markdown',
